f69fb8e58492407901767deab8d7ba7a23742c68
Pushkar Aditya <pushkar.aditya@gmail.com>
2020-05-12 00:33:17 +0530

    working on base conditions for TOH (Tower of Hanoi)

--- chapter5/TowerOfHanoi.py
+++ chapter5/TowerOfHanoi.py
@@ -15,14 +15,21 @@ class Disk():
 
 
 class Pin():
-    def __init__(self):
+    def __init__(self, name):
         self.stack = Stack()
+        self.label = name
+
+    def name(self):
+        return self.label
 
     def isEmpty(self):
         return self.stack.isEmpty()
 
     def check(self, d):
         return self.stack.isEmpty() or d.get() < self.stack.peek().get()
+
+    def peek(self):
+        return self.stack.peek()
 
     def push(self, d):
         if self.check(d):
@@ -35,13 +42,39 @@ class Pin():
         return self.stack.pop()
 
     def __str__(self):
-        return str(self.stack)
+        return self.name() + "=>" + str(self.stack)
 
+def moveOne(From, To):
+    if To.check(From.peek()):
+        d = From.pop()
+        print ('Moving {} from {} to {}'.format(d, From.name(), To.name()))
+        To.push(d)
+        return True
+    else:
+        return False
 
-# pin = Pin()
-# pin.push(Disk(2))
-# # print(pin)
-# pin.push(Disk(1))
-# print(pin)
-# print(pin.push(Disk(13)))
-# print(pin)
+fp = Pin('FP')
+wp = Pin('WP')
+tp = Pin('TP')
+
+def setUp(n):
+    for i in range(n, 0, -1):
+        fp.push(Disk(i)) 
+    print(fp)
+    print('set up completed')
+    print()
+n = 2
+setUp(n)
+
+def logic(n):
+    if n == 1:
+        moveOne(fp, tp)
+    if n == 2:
+        moveOne(fp, wp)
+        moveOne(fp, tp)
+        moveOne(wp, tp)
+
+    print()
+    print(tp)
+
+logic(n)
