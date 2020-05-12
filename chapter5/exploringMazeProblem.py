b76807303e962a437c5f33dc259046d1f8fabeda
Pushkar Aditya <pushkar.aditya@gmail.com>
2020-05-12 20:10:31 +0530

    completed with basic testing

--- chapter5/exploringMazeProblem.py
+++ chapter5/exploringMazeProblem.py
@@ -14,6 +14,7 @@ class Maze():
 
     def __init__(self, turtle, maze):
         self.m = maze
+        self.cm = None
         self.t = turtle
         self.pos = self.t.pos()
         self.start = None
@@ -71,16 +72,64 @@ class Maze():
         self.cm = [[v for v in row] for row in self.m] # copied maze
 
     def evaluate(self, i, j):
-        pass
+        # 4 points, clock wise
+        # check if already evaluated
+        # if not, evaluate near by
+            # it could be wall
+                # do not move forward
+            # it could be red
+                # do not move forward
+            # it could be green
+                # do not move forward
+            # it could be wip
+                # do not move forward, it will be evaluated automatically later
+            # it could be open
+                # is it boundry
+                    # mark it green
+                # else yellow and move forward
+        # print('evaluating ({}, {})'.format(i, j))
+        cv = self.cm[i][j]
+        if cv in [blockCell, 'red', 'green', 'yellow']:
+            # print('({}, {}) => {}'.format(i, j, cv))
+            return cv
+        elif cv == openCell:
+            if i == 0 or j == 0 or i == len(self.cm) - 1 or j == len(self.cm[i]) - 1:
+                # print('({}, {}) => green for boundary condition'.format(i, j))
+                self.cm[i][j] = 'green'
+                # print(self.cm)
+                self.drawCell(i, j, 'green')
+                return 'green'
+            else:
+                self.cm[i][j] = 'yellow'
+                self.drawCell(i, j, 'yellow')
+                # l = self.evaluate(i, j - 1)
+                # u = self.evaluate(i - 1, j)
+                # r = self.evaluate(i, j + 1)
+                # d = self.evaluate(i + 1, j)
+                # # print(l, u, r, d)
+                # if 'green' in [l, u, r, d]:
+                if  self.evaluate(i, j - 1) == 'green' or self.evaluate(i - 1, j) == 'green' or self.evaluate(i, j + 1) == 'green' or self.evaluate(i + 1, j) == 'green':
+                    self.cm[i][j] = 'green'
+                    # print(self.cm)
+                    self.drawCell(i, j, 'green')
+                    return 'green'
+                else:
+                    self.cm[i][j] = 'red'
+                    # print(self.cm)
+                    self.drawCell(i, j, 'red')
+                    return 'red'
+
 
     def solve(self):
-        # i, j = self.start
+        i, j = self.start
         self.copyMaze()
-        print(self.cm)
-        self.drawCell(1, 1, self.wip)
-        self.drawCell(2, 1, self.wip)
-        self.drawCell(2, 1, self.path)
-        self.drawCell(1, 1, self.path)
+        self.cm[i][j] = openCell
+        self.evaluate(i, j)
+
+        # self.drawCell(1, 1, self.wip)
+        # self.drawCell(2, 1, self.wip)
+        # self.drawCell(2, 1, self.path)
+        # self.drawCell(1, 1, self.path)
 
 
 
@@ -96,13 +145,13 @@ def main():
     x = blockCell
     S = startingCell
     o = openCell
-    # m = [
-    #         [x,x,x,x,x],
-    #         [x,o,o,o,x],
-    #         [o,o,x,o,x],
-    #         [x,x,o,o,x],
-    #         [x,x,o,x,x]
-    # ]
+    m = [
+            [x,x,x,x,x],
+            [x,o,o,o,x],
+            [x,S,x,o,x],
+            [x,x,o,o,x],
+            [x,x,o,x,x]
+    ]
     # m = [
     #         [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x],
     #         [x,o,o,o,x,o,o,o,x,x,o,x,x,o,o,o,o,o,x,o,o,o],
@@ -116,11 +165,11 @@ def main():
     #         [x,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,x,o,x,x,x],
     #         [x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,o,x,x,x],
     #     ]
-    m = [
-        [x,x,x],
-        [x,S,x],
-        [x,o,x]
-    ]
+    # m = [
+    #     [x,x,x],
+    #     [x,S,x],
+    #     [x,o,x]
+    # ]
 
     m = Maze(t, m)
 
