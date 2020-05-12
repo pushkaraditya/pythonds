a1914c7a25bbbb04c6b029efa6dcbbfc9cce1002
Pushkar Aditya <pushkar.aditya@gmail.com>
2020-05-12 11:45:12 +0530

    completed tower of Hanoi problem

--- chapter5/TowerOfHanoi.py
+++ chapter5/TowerOfHanoi.py
@@ -3,6 +3,9 @@ sys.path.append('..')
 
 from basic.stack import Stack
 
+class Globals():
+    moveNumber = 0
+
 class Disk():
     def __init__(self, size):
         self.size = size
@@ -44,37 +47,52 @@ class Pin():
     def __str__(self):
         return self.name() + "=>" + str(self.stack)
 
+
+
 def moveOne(From, To):
     if To.check(From.peek()):
         d = From.pop()
-        print ('Moving {} from {} to {}'.format(d, From.name(), To.name()))
+        Globals.moveNumber = Globals.moveNumber + 1
+        print ('{}. Moving {} from {} to {}'.format(Globals.moveNumber, d, From.name(), To.name()))
         To.push(d)
         return True
     else:
         return False
 
-fp = Pin('FP')
-wp = Pin('WP')
-tp = Pin('TP')
+A = Pin('Pin A')
+B = Pin('Pin B')
+C = Pin('Pin C')
+pins = [A,B,C]
 
-def setUp(n):
+def setUp(fp, n):
     for i in range(n, 0, -1):
         fp.push(Disk(i)) 
     print(fp)
-    print('set up completed')
+    print('set up completed. {0} has {1} disks'.format(fp.name(), n))
     print()
-n = 2
-setUp(n)
 
-def logic(n):
+def calculateWP(pins, fp, tp):
+    tmp = [p for p in pins]
+    tmp.remove(fp)
+    tmp.remove(tp)
+    return tmp[0]
+
+def logic(pins, fp, tp, n):
     if n == 1:
         moveOne(fp, tp)
-    if n == 2:
-        moveOne(fp, wp)
+        return 1
+    else:
+        wp = calculateWP(pins, fp, tp)
+        moves = logic(pins, fp, wp, n - 1)
         moveOne(fp, tp)
-        moveOne(wp, tp)
+        moves += logic(pins, wp, tp, n - 1)
+        return moves + 1
 
-    print()
-    print(tp)
 
-logic(n)
+n = 16
+setUp(A, n)
+
+moves = logic(pins, A, C, n)
+print()
+print('it has taken {0} moves'.format(moves))
+print(C)
